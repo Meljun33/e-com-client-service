d4001070b0600eb8e3491a22b714c27a1ee07aa7
Meljun33 <meljun.canizo33@gmail.com>
2025-03-30 17:44:10 +0800

    Commit

---
+++ src/api/Index.js
@@ -0,0 +1,19 @@
+import axios from  'axios'
+
+export default {
+    http (baseUrl){
+        const http = axios.create({baseURL: baseUrl})
+
+        http.interceptors.request.use((config)=>{
+            const token = "";
+            
+            config.headers.Accept = "application/json";
+            config.headers.Authorization = 'Bearer ${Token}';
+
+            return config;
+
+        })
+
+    }
+
+}
---
+++ src/api/auth/auth.js
@@ -0,0 +1,30 @@
+import axios from "axios";
+
+http (baseUrl){
+    const http = axios.create({baseURL: baseUrl})
+
+    http.interceptors.request.use((config)=>{
+        const token = "";
+
+        config.headers.Accept = "application/json";
+        config.headers.Authorization = 'Bearer ${Token}';
+
+        return config;
+
+    });
+
+    return http;
+
+};
+
+export default{
+    baseUrl: 'http://localhost:8000',
+    endpoit: "/api",
+
+    login(param){
+
+        return http(this.baseUrl).post(`${this.endpoint}/login`, params);
+    },
+
+};
+

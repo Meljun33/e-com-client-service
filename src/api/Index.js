1aeeb41f30f197029bc985a075fdf1c5d679b260
Meljun33 <meljun.canizo33@gmail.com>
2025-03-31 09:40:19 +0800

    next

--- src/api/Index.js
+++ src/api/Index.js
@@ -5,10 +5,10 @@ export default {
         const http = axios.create({baseURL: baseUrl})
 
         http.interceptors.request.use((config)=>{
-            const token = "";
+            const token = JSON.parse(localStorage.getItem("token"));
 
             config.headers.Accept = "application/json";
-            config.headers.Authorization = 'Bearer ${Token}';
+            config.headers.Authorization = 'Bearer ${Token.access_token}';
 
             return config;
 
